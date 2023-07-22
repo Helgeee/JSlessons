1bad256a03412aa12a882b405edbfd2ef750f0be
Oleg <oleg@gmail.com>
2023-07-22 16:03:50 +0300

    add js016

---
+++ js NEW START/js016/script.js
@@ -0,0 +1,26 @@
+
+
+'use strict';
+
+function first(){
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
+
+function second(){
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback){
+    console.log(`Я учу : ${ lang }`);
+    callback();
+}
+function done(){
+    console.log(`ya prochel etot urock!`)
+}
+
+learnJS(`JavaScript`, done );
